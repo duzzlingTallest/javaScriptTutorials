c86e48a5bb083d9bdc8d6730286ff3fb2aa850f5
duzzlingTallest <kaushaldamani386@gmail.com>
2022-09-10 13:20:29 +0530

    object

---
+++ Object.js
@@ -0,0 +1,11 @@
+const person = {
+    firstName: 'Kaushal',
+    lastName: 'Damani',
+    age: '19',
+    eyeColor: 'black',
+  };
+  
+  /*we can also right this way :- 
+  const person={firstName:"Kaushal",lastName:"Damani",age:"19",eyeColor:"black"};*/
+  
+  console.log(person);
--- README.md
+++
@@ -1,3 +0,0 @@
-# javaScriptTutorials
- 
-test commit
