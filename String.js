2f164b96fe6a2f404a8a29baf0e50249685c082b
duzzlingTallest <kaushaldamani386@gmail.com>
2022-09-12 20:36:24 +0530

    javaScript

---
+++ String.js
@@ -0,0 +1,22 @@
+/*String :- JavaScript strings are for storing and manipulating text. */
+
+/*A JavaScript string is zero or more characters written inside quotes.*/
+
+/* We can use '' , "" both quotes */
+
+//CODE :-
+
+// let carName1='BMW X7'
+// let carName2="BMW X7"
+
+// console.log(carName1,carName2);
+
+/* -->  We can use quotes inside a string, aas long as they don't match the quotes surrounding the string */
+
+//CODE :-
+
+let answer1 = " It's alright\n";
+let answer2 = "He is called 'Kaushal'\n";
+let answer3 = 'He is called "Kaushal"\n';
+
+console.log(answer1, answer2, answer3);
---
+++ String_Length.js
@@ -0,0 +1,23 @@
+/* -->  String Length :- To find length of a string, use to built-in length property.
+
+    --> It also calculate 'space' between word. 
+*/
+
+// const text = "My Name Is kaushal Damani";
+
+// console.log(text.length);
+
+/*  -->  Escaps Character :- Because sstring must be written within quotes, JavaScript will misunderstand this string.
+ */
+
+// this type of code create error.
+
+// const text = "We are the so-called "Viking" from the north.";
+
+// console.log(text);
+
+// -----> but when use the '',\',\",",\\,\ this quotes so we can run easily.
+
+// const text = "We are the so-called 'Viking' from the north.";
+
+// console.log(text);
