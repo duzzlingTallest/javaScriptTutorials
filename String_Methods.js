291bb3e7b59c31acb9571656950c90cbb479c985
duzzlingTallest <kaushaldamani386@gmail.com>
2022-09-13 22:36:47 +0530

    String as Object

---
+++ String_Methods.js
@@ -0,0 +1,15 @@
+// --->  String Methods : it helps you to work with string.
+
+/*String Methods and Properties :--
+        Primitive values, like "John Doe", cannot have properties or methods (because they are not objects).
+
+        But with JavaScript, methods and properties are also available to primitive values, because JavaScript treats primitive values as objects when executing methods and properties.
+
+*/
+
+    // ---> JavaScript String Length : The length property returns the length of a string :
+
+    let txt ='abcdefghijklmnopqrstuvwxyz';
+    let length =txt.length;
+
+    console.log(txt.length);
--- String_as_Object.js
+++ String_as_Object.js
@@ -8,3 +8,13 @@
 
 // ---> But string can also be defined as objects with the Keyword 'new'.
 
+// const x = 'Kaushal';
+const y = new string("kaushal");
+
+console.log(x,y);
+
+/*  --=>Do not create Strings objects.
+
+The new keyword complicates the code and slows down execution speed.
+
+String objects can produce unexpected results:  */
