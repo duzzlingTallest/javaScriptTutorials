07b52f98cab8e13a43f96aeaf8dc2502e4a88209
duzzlingTallest <kaushaldamani386@gmail.com>
2022-09-17 23:03:13 +0530

    Converting_to_upperCase_and_lowerCase

---
+++ Converting_to_upperCase_and_lowerCase.js
@@ -0,0 +1,18 @@
+//   Converting to Upper and Lower Case :- 
+
+/* A string is converted to upper case with toUpperCase():
+
+A string is converted to lower case with toLowerCase():
+*/
+
+// JavaScript String toUpperCase() :-
+
+//   let text1 = "Hello World!!"
+
+//   console.log(text1.toUpperCase())
+
+// JavaScript String toLowerCase() :--
+
+// let text1 = "Hello World!!"
+
+//   console.log(text1.toLowerCase())
