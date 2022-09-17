1629ae40c2778c4c6401fe401798bae7ce5307d6
duzzlingTallest <kaushaldamani386@gmail.com>
2022-09-17 23:03:41 +0530

    String_concat

---
+++ String_concat.js
@@ -0,0 +1,40 @@
+// ---> JavaScript String concat().  <---
+
+// 1} MATHODE
+
+// concat() joins two or more string : 
+
+// EX:-
+
+//   const text1 = "Hello";
+//   const text2 = "World";
+
+//   console.log(text1.concat("",text2))
+
+// 2} MATHODE
+
+// The concat() method can be used instead of the plus operator. These two lines do the same:
+
+//EX:-
+
+//    text ="Hello" + " " + "World!";
+
+//    console.log("Hello".concat(" ","World!"));
+
+// NOTE:-  
+
+/* All string methods return a new string. They don't modify the original string.
+
+Formally said:
+
+Strings are immutable: Strings cannot be changed, only replaced.
+*/
+
+// ---> JavaScript String trim(). <---
+
+// --> The trim() method removes whitespace from both sides of a string:
+
+// let text1 = "     Hello World!!       ";
+
+// console.log(text1.trim())
+
